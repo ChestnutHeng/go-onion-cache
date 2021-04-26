4f4021a0493380b650998bd157a8da0fbb7d9caf
lvziheng <lvziheng@bytedance.com>
2021-04-26 17:27:09 +0800

    up

---
+++ tinyutil/json.go
@@ -0,0 +1,25 @@
+package tinyutil
+
+import "encoding/json"
+
+func MarshalToString(i interface{}) string {
+	if i == nil {
+		return ""
+	}
+	data, err := json.Marshal(i)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+func MarshalToValue(obj interface{}) string {
+	switch v := obj.(type) {
+	case string:
+		return v
+	case *string:
+		return *v
+	default:
+		return MarshalToString(v)
+	}
+}
